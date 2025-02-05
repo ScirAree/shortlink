17f9b1f61f4b554d366c895d11111ea6275d468a
aree <whr_zm@163.com>
2025-02-05 15:36:51 +0800

    1

---
+++ admin/src/main/java/org/nageoffer/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,14 @@
+package org.nageoffer.shortlink.admin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author aree
+ * 2025/2/5
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
